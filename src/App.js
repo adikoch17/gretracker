4afab630f517dcc3d2d049fdf65d14e0290a8bd5
aditya kocherlakota <aditya.kocherlakota@gmail.com>
2022-01-15 19:56:19 +0530

    shifted server

--- src/App.js
+++ src/App.js
@@ -5,39 +5,119 @@ import IconButton from '@mui/material/IconButton';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import {indigo} from '@mui/material/colors';
 import Tooltip from '@mui/material/Tooltip';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function App() {
 
   const [daily,setDaily] = useState([])
   const [total,setTotal] = useState([])
+  const [open, setOpen] = React.useState(false);
 
-  const getDaily = () =>{
-    fetch('https://Grebot2.adityakocherlak.repl.co/daily')
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // const getDaily = async () =>{
+  //   fetch('https://Grebot2.adityakocherlak.repl.co/daily')
+  //   .then(response => response.json())
+  //   .then(data=>{
+  //     console.log(Object.keys(data).length)
+  //     var x = data.map(item=>[item.hours,item.time])
+  //     setDaily(x)
+  //     console.log(x)
+  //     return new Promise((resolve,reject)=>{
+  //       if(x){
+  //         resolve(true)
+  //       }
+  //       else{
+  //         console.log("daily rejected")
+  //         reject(false)
+  //       }
+  //     })
+  //   })
+  // }
+
+  // const getTotal = async () =>{
+    // fetch('https://Grebot2.adityakocherlak.repl.co/total')
+    // .then(response => response.json())
+    // .then(data=>{
+    //   console.log(Object.keys(data).length)
+    //   var x = data.map(item=>[item.total,item.time])
+    //   setTotal(x)
+    //   console.log(x)
+    //   return new Promise((resolve,reject)=>{
+    //     if(x){
+    //       resolve(true)
+    //     }
+    //     else{
+    //       console.log("total rejected")
+    //       reject(false)
+    //     }
+    //   })
+    // })
+  // }
+
+
+  // const getDaily = new Promise((resolve,reject)=>{
+  //   fetch('https://Grebot2.adityakocherlak.repl.co/daily')
+  //   .then(response => response.json())
+  //   .then(data=>{
+  //     console.log(Object.keys(data).length)
+  //     var x = data.map(item=>[item.hours,item.time])
+  //     setDaily(x)
+  //     console.log(x)
+  //     resolve(true)
+  //   })
+  //   .catch(err=>{
+  //     reject(false)
+  //   })
+  // })
+
+  // const getTotal  = new Promise((resolve,reject)=>{
+  //   fetch('https://Grebot2.adityakocherlak.repl.co/total')
+  //     .then(response => response.json())
+  //     .then(data=>{
+  //       console.log(Object.keys(data).length)
+  //       var x = data.map(item=>[item.total,item.time])
+  //       setTotal(x)
+  //       console.log(x)
+  //       resolve(true)
+  //     })
+  //     .catch(err=>{
+  //       reject(false)
+  //     })
+  // })
+
+  const getData = () =>{
+    fetch('https://gretracker-server.herokuapp.com/daily')
     .then(response => response.json())
     .then(data=>{
       console.log(Object.keys(data).length)
       var x = data.map(item=>[item.hours,item.time])
-      // var x = data.map(item=>{return {"x":item.hours,"y":item.time}})
-      setDaily(x)
-      console.log(x)
+      return x
     })
-  }
-
-  const getTotal = () =>{
-    fetch('https://Grebot2.adityakocherlak.repl.co/total')
-    .then(response => response.json())
-    .then(data=>{
-      console.log(Object.keys(data).length)
-      var x = data.map(item=>[item.total,item.time])
-      // var x = data.map(item=>{return {"x":item.total,"y":item.time}})
-      setTotal(x)
-      console.log(x)
+    .then(y=>{
+      fetch('https://gretracker-server.herokuapp.com/total')
+      .then(response => response.json())
+      .then(data=>{
+        console.log(Object.keys(data).length)
+        var x = data.map(item=>[item.total,item.time])
+        setDaily(y)
+        setTotal(x)
+        console.log(y)
+        console.log(x)
+        handleClose()
+      })
     })
   }
 
   const refreshData = ()=>{
-    getDaily()
-    getTotal()
+    handleOpen()
+    getData()
   }
 
   useEffect(()=>{
@@ -47,6 +127,9 @@ function App() {
   return (
     <div className="App">
       {/* <button onClick={refreshData}>refreshData</button> */}
+      <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <div id="refresh">
       <Tooltip title="Refresh">
         <IconButton onClick={refreshData} aria-label="refresh">
