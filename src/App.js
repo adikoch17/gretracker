9b3b9031357d0b6177c0931e6d70978e8bd1e152
aditya kocherlakota <aditya.kocherlakota@gmail.com>
2022-01-16 14:28:27 +0530

    added error snackbar

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import {indigo} from '@mui/material/colors';
 import Tooltip from '@mui/material/Tooltip';
 import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 function App() {
 
   const [daily,setDaily] = useState([])
   const [total,setTotal] = useState([])
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen]  =  useState(false);
+  const [errorOpen,setErrorOpen] = useState(false)
+  const [error,setError] = useState("")
 
   const handleClose = () => {
     setOpen(false);
@@ -21,76 +25,14 @@ function App() {
     setOpen(true);
   };
 
-  // const getDaily = async () =>{
-  //   fetch('https://Grebot2.adityakocherlak.repl.co/daily')
-  //   .then(response => response.json())
-  //   .then(data=>{
-  //     console.log(Object.keys(data).length)
-  //     var x = data.map(item=>[item.hours,item.time])
-  //     setDaily(x)
-  //     console.log(x)
-  //     return new Promise((resolve,reject)=>{
-  //       if(x){
-  //         resolve(true)
-  //       }
-  //       else{
-  //         console.log("daily rejected")
-  //         reject(false)
-  //       }
-  //     })
-  //   })
-  // }
+  const openError = () =>{
+    setErrorOpen(true)
+  }
 
-  // const getTotal = async () =>{
-    // fetch('https://Grebot2.adityakocherlak.repl.co/total')
-    // .then(response => response.json())
-    // .then(data=>{
-    //   console.log(Object.keys(data).length)
-    //   var x = data.map(item=>[item.total,item.time])
-    //   setTotal(x)
-    //   console.log(x)
-    //   return new Promise((resolve,reject)=>{
-    //     if(x){
-    //       resolve(true)
-    //     }
-    //     else{
-    //       console.log("total rejected")
-    //       reject(false)
-    //     }
-    //   })
-    // })
-  // }
+  const closeError = () =>{
+    setErrorOpen(false)
+  }
 
-
-  // const getDaily = new Promise((resolve,reject)=>{
-  //   fetch('https://Grebot2.adityakocherlak.repl.co/daily')
-  //   .then(response => response.json())
-  //   .then(data=>{
-  //     console.log(Object.keys(data).length)
-  //     var x = data.map(item=>[item.hours,item.time])
-  //     setDaily(x)
-  //     console.log(x)
-  //     resolve(true)
-  //   })
-  //   .catch(err=>{
-  //     reject(false)
-  //   })
-  // })
-
-  // const getTotal  = new Promise((resolve,reject)=>{
-  //   fetch('https://Grebot2.adityakocherlak.repl.co/total')
-  //     .then(response => response.json())
-  //     .then(data=>{
-  //       console.log(Object.keys(data).length)
-  //       var x = data.map(item=>[item.total,item.time])
-  //       setTotal(x)
-  //       console.log(x)
-  //       resolve(true)
-  //     })
-  //     .catch(err=>{
-  //       reject(false)
-  //     })
-  // })
 
   const getData = () =>{
     fetch('https://gretracker-server.herokuapp.com/daily')
@@ -111,7 +53,24 @@ function App() {
         console.log(y)
         console.log(x)
         handleClose()
+        if(x.length == 0 && !errorOpen){
+          setError("No data is available")
+          openError()
+          setTimeout(() => {
+            closeError()
+          }, 10000);
+        }
       })
+    })
+    .catch(err =>{
+      setError("Error while fetching data")
+      console.log("error in loading data")
+      handleClose()
+      openError()
+      setTimeout(() => {
+        closeError()
+      }, 10000);
+
     })
   }
 
@@ -127,6 +86,11 @@ function App() {
   return (
     <div className="App">
       {/* <button onClick={refreshData}>refreshData</button> */}
+      <Snackbar open={errorOpen} autoHideDuration={6000} onClose={closeError}>
+        <Alert onClose={closeError} severity="error" sx={{ width: '100%'}} variant="filled">
+          <b>Error: </b>{error}
+        </Alert>
+      </Snackbar>
       <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} open={open}>
         <CircularProgress color="inherit" />
       </Backdrop>
